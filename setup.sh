bd02238efc930948d1b72947ebac9dc6841a1fdf
rivayama <tomozo16@gmail.com>
2014-05-05 17:23:32 +0900

    modify to install vim vundle and vim plugins

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ for dotfile in .?*
 do
   if [ $dotfile != '..' ] && [ $dotfile != '.git' ] && [ $dotfile != '.gitmodules' ] && [ $dotfile != '.gitignore' ]
   then
-    ln -Fis "$PWD/$dotfile" $HOME
+    ln -Ffs "$PWD/$dotfile" $HOME
   fi
 done
+
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+vim  +BundleInstall +q
